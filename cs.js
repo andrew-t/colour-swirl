24a9bf756f255010d71b19a59f0b7e90958d6717
Andrew Taylor <Andrew.Taylor04@bbc.co.uk>
2017-10-29 21:15:51 +0000

    Initial commit

---
+++ cs.js
@@ -0,0 +1,88 @@
+window.addEventListener('resize', setSize);
+document.addEventListener('DOMContentLoaded', setSize);
+function setSize(e) {
+	const scale = window.innerHeight / window.innerWidth;
+	const els = document.getElementsByClassName('background');
+	for (let i = 0; i < els.length; ++i)
+		els[i].style.transform = `scale(1, ${scale})`;
+}
+
+let lastFrame,
+	t;
+const rects = [];
+document.addEventListener('DOMContentLoaded', firstFrame);
+function firstFrame() {
+	lastFrame = Date.now();
+	for (let i = 1; true; ++i) {
+		const rect = document.getElementById(`c${i}`);
+		if (!rect) break;
+		rects.push({
+			rect,
+			gradient: document.getElementById(`g${i}`),
+			endStop: document.getElementById(`s${i}`),
+			hue: {
+				omega: Math.random() * 0.0001 + 0.00003,
+				phase: Math.random()
+			}
+		});
+	}
+	drawFrame(t = 0);
+	window.requestAnimationFrame(frame);
+}
+function frame() {
+	const now = Date.now(),
+		dt = Math.min(now - lastFrame, 150);
+	lastFrame = now;
+	drawFrame(t += dt);
+	window.requestAnimationFrame(frame);
+}
+
+function drawFrame(t) {
+	const hues = rects.map(rect => rect.hue);
+	hues.forEach(v => {
+		v.value = t * v.omega + v.phase;
+		v.rgb = hslToRgb(v.value % 1, 0.5, 0.5);
+	});
+	for (let i = 0; i < 3; ++i) {
+		let total = hues.reduce((c, n) => c + n.rgb[i], 0),
+			min = Math.min(...hues.map(v => v.rgb[i])),
+			max = Math.max(...hues.map(v => v.rgb[i]));
+		if (min == max) {
+			min -= 0.1;
+			max += 0.1;
+		}
+		// hues.forEach(v => v.rgb[i] = Math.round(v.rgb[i] * 255 / total));
+		// hues.forEach(v => v.rgb[i] = Math.round((v.rgb[i] - min) * 255 / (total - min * 3)));
+		// hues.forEach(v => v.rgb[i] = Math.round((v.rgb[i] - min) * 255 / (max - min)));
+		hues[hues.length - 1].rgb[i] = 255 - (total - hues[hues.length - 1].rgb[i]);
+	}
+	rects.forEach(r => {
+		r.endStop.setAttribute('stop-color',
+			`rgb(${r.hue.rgb.join(', ')})`);
+	});
+}
+
+// https://stackoverflow.com/a/9493060/1491108
+function hslToRgb(h, s, l){
+    var r, g, b;
+
+    if (s == 0)
+        r = g = b = l; // achromatic
+    else {
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1 / 3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
+}
+function hue2rgb(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1 / 6) return p + (q - p) * 6 * t;
+    if (t < 1 / 2) return q;
+    if (t < 2 / 3) return p + (q - p) * (2/3 - t) * 6;
+    return p;
+}
